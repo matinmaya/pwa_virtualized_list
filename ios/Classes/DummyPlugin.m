bf79fa76f3962b90e389ecdeb1d73d81e82de18a
Matin <matinm53@gmail.com>
2025-05-27 09:11:55 +0700

    add support ios plugin

---
+++ ios/Classes/DummyPlugin.m
@@ -0,0 +1,12 @@
+#import <Flutter/Flutter.h>
+
+@interface DummyPlugin : NSObject <FlutterPlugin>
+@end
+
+@implementation DummyPlugin
+
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+    // Placeholder implementation
+}
+
+@end
